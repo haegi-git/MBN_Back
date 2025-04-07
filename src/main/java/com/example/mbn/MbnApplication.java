34fd17c1c05897dc63f15b8fb86ae92ab991b9b1
haegi <b_haegi@naver.com>
2025-04-07 13:23:18 +0900

    프로젝트 생성 및 DB연결

---
+++ src/main/java/com/example/mbn/MbnApplication.java
@@ -0,0 +1,13 @@
+package com.example.mbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbnApplication.class, args);
+	}
+
+}
